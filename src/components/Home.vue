7298318728da7d53f1c84634fd846881f9d36763
Manish <manishkarkera@gmail.com>
2021-07-22 15:35:49 +1000

    wire up API

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,22 @@
 <template>
   <h2>{{ msg }}</h2>
+  <template v-for="character in characters" :key="character.id">
+    <li>{{ character.name }}</li>
+  </template>
 </template>
 
 <script>
 export default {
   name: "Home",
   data: () => ({
-    msg: "Lets goooo!!"
-  })
+    pageNo: 1,
+    msg: "Lets goooo!!",
+    characters: []
+  }),
+  mounted() {
+    fetch(`https://rickandmortyapi.com/api/character/?page=${this.pageNo}`)
+      .then(response => response.json())
+      .then(({ results }) => this.characters = results )
+  }
 }
 </script>
